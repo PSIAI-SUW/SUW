ccecb4abe22cc62038596419d349b82896d51efc
Adam Kanigowski <ka31544@zut.edu.pl>
2018-11-12 23:01:21 +0100

    Update stats.php, nowy plik settings.php

--- www/stats.php
+++ www/stats.php
@@ -1,7 +1,7 @@
 <?php
 
 /* Połączenie z bazą danych */
-$dbHost = 'localhost';
+$dbHost = 'mysql.cba.pl';
 $dbUser = 'BazaISUW';
 $dbPassword = 'BazaISUW1';
 $dbName = 'jakub9616';
@@ -22,19 +22,19 @@ $results = $mysqli->query("SELECT COUNT(ID_USER) FROM users WHERE type_account='
 $row = mysqli_fetch_array($results, MYSQLI_NUM);
 
 /* Zapytanie SQL zliczające pliki z "dzisiaj" */
-$download_today = $mysqli->query("SELECT COUNT(ID_Pobieranie_Pliku) FROM pobranie_pliku WHERE DAY(data_dodania)=$today");
+$download_today = $mysqli->query("SELECT COUNT(ID_Pobieranie_Pliku) FROM Pobranie_Pliku WHERE DAY(data_dodania)=$today");
 $download_today2 = mysqli_fetch_array($download_today);
 
 /* Zapytanie SQL zliczające pliki z tygodnia */
-$download_week = $mysqli->query("SELECT COUNT(ID_Pobieranie_Pliku) FROM pobranie_pliku WHERE WEEK(data_dodania,5)=$week AND YEAR(data_dodania)=$year");
+$download_week = $mysqli->query("SELECT COUNT(ID_Pobieranie_Pliku) FROM Pobranie_Pliku WHERE WEEK(data_dodania,5)=$week AND YEAR(data_dodania)=$year");
 $download_week2 = mysqli_fetch_array($download_week, MYSQLI_NUM);
 
 /* Zapytanie SQL zliczające pliki z miesiaca */
-$download_month = $mysqli->query("SELECT COUNT(ID_Pobieranie_Pliku) FROM pobranie_pliku WHERE MONTH(data_dodania)=$month");
+$download_month = $mysqli->query("SELECT COUNT(ID_Pobieranie_Pliku) FROM Pobranie_Pliku WHERE MONTH(data_dodania)=$month");
 $download_month2 = mysqli_fetch_array($download_month, MYSQLI_NUM);
 
 /* Zapytanie SQL zliczające pliki z semestru */
-$download_semestr = $mysqli->query("SELECT COUNT(ID_Pobieranie_Pliku) FROM pobranie_pliku");
+$download_semestr = $mysqli->query("SELECT COUNT(ID_Pobieranie_Pliku) FROM Pobranie_Pliku");
 $download_semestr2 = mysqli_fetch_array($download_semestr, MYSQLI_NUM);
 
 ?>
