9ba953633cef12091db5298296bf2cd5603b8350
Jakub Piskorowski <jakub9616@gmail.com>
2018-11-22 06:50:23 +0100

    Dodanie wykladu do bazy danych

--- www/sendwyklad.php
+++ www/sendwyklad.php
@@ -7,5 +7,8 @@ if(is_uploaded_file($plik_tmp)) {
      move_uploaded_file($plik_tmp, "uploadwyklad/$plik_nazwa"); 
     echo "Plik: <strong>$plik_nazwa</strong> o rozmiarze 
     <strong>$plik_rozmiar bajtów</strong> dodano."; 
+	
+	$send_lecture_sql = "insert into `Dodanie_Pliku` (`ID_Dodanie_Pliku`, `data_dodania`, `nr_kurs`, `nr_plik` , `nr_user`) VALUES ('', '2018-01-01', '1', '1', '6')";
+	$send_lecture = mysql_query($send_lecture_sql);
 } 
 ?> 
