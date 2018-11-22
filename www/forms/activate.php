84ea03f7f361ed79044b3e7ba1d7d7ce520e3ad5
Krzysztof Kuzak <k.kuzak97@gmail.com>
2018-11-22 11:01:53 +0100

    Powiadomienia o błędach

--- www/forms/activate.php
+++ www/forms/activate.php
@@ -2,13 +2,19 @@
 
 require_once '../config.php';
 
-if(isset($_POST['activateButton']))
+if(empty($_POST['activateButton']))
 {
-    $activate = $_POST['activate'];
-    $query = "UPDATE users SET active = 'aktywny' WHERE login = '$activate'";
-    $stmt = $db->query($query);
+	if(isset($_POST['activateButton']))
+	{
+		$activate = $_POST['activate'];
+		$query = "UPDATE users SET active = 'aktywny' WHERE login = '$activate'";
+		$stmt = $db->query($query);
+	}
+
+	header('Location: ../settings_admin.php?not=2');
 }
-
-header('Location: ../settings_admin.php?not=2');
-
+else
+{
+	header('Location: ../settings_admin.php?error=10');
+}
 ?>
--- www/forms/deactivate.php
+++ www/forms/deactivate.php
@@ -2,13 +2,20 @@
 
 require_once '../config.php';
 
-if(isset($_POST['deactivateButton']))
+if(empty($_POST['deactivateButton']))
 {
-    $deactivate = $_POST['deactivate'];
-    $query = "UPDATE users SET active = 'nieaktywny' WHERE login = '$deactivate'";
-    $stmt = $db->query($query);
+	if(isset($_POST['deactivateButton']))
+	{
+		$deactivate = $_POST['deactivate'];
+		$query = "UPDATE users SET active = 'nieaktywny' WHERE login = '$deactivate'";
+		$stmt = $db->query($query);
+	}
+
+	header('Location: ../settings_admin.php?not=3');
 }
-
-header('Location: ../settings_admin.php?not=3');
-
+else
+{
+	header('Location: ../settings_admin.php?error=10');
+}
+	
 ?>
