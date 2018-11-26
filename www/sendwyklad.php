9aa95dfbd2d4472442d34d40e84d3fa4d14a0b62
Kucharczyk Kamil <kk36814@zut.edu.pl>
2018-11-26 21:33:16 +0100

    wysylanie wykladu

--- www/sendwyklad.php
+++ www/sendwyklad.php
@@ -1,14 +1,48 @@
 <?php 
+	require_once("config.php");
+
 $plik_tmp = $_FILES['plik']['tmp_name']; 
 $plik_nazwa = $_FILES['plik']['name']; 
 $plik_rozmiar = $_FILES['plik']['size']; 
 
-if(is_uploaded_file($plik_tmp)) { 
+
+if(is_uploaded_file($plik_tmp)) 
+{ 
      move_uploaded_file($plik_tmp, "uploadwyklad/$plik_nazwa"); 
-    echo "Plik: <strong>$plik_nazwa</strong> o rozmiarze 
-    <strong>$plik_rozmiar bajtów</strong> dodano."; 
+   // echo "Plik: <strong>$plik_nazwa</strong> o rozmiarze
+    //<strong>$plik_rozmiar bajtów</strong> dodano.";
+    header('Location: main.php?not=16');
+
+
 	
-	$send_lecture_sql = "insert into `Dodanie_Pliku` (`ID_Dodanie_Pliku`, `data_dodania`, `nr_kurs`, `nr_plik` , `nr_user`) VALUES ('', '2018-01-01', '1', '1', '6')";
-	$send_lecture = mysql_query($send_lecture_sql);
+	$sql = "SELECT MAX(ID_Plik) AS id FROM Plik;";
+	$stmt = $db->prepare($sql);
+	$stmt->execute();
+	$id = $stmt->fetch(PDO::FETCH_ASSOC);
+	$id = (isset($id['id'])) ? $id['id'] : 0; 
+
+
+
+
+	$sql = "insert into `Plik` (`nazwa`, `nr_pliku` , `sciezka`) VALUES (:name,:nplik, :path)";
+	$stmt = $db->prepare($sql);
+	$stmt->execute(array(
+		"name"=>$plik_nazwa, 
+		"nplik"=>($id+1), 
+		"path"=>"uploadwyklad/$plik_nazwa"));
+
+
+
+	
+	$sql = "insert into `Dodanie_Pliku` (`data_dodania`, `nr_kurs`, `nr_plik` , `nr_user`) VALUES (NOW(),:nkurs, :nplik, :nuser)"; 
+	
+	$stmt = $db->prepare($sql);
+	$stmt->execute(array(
+		"nkurs"=>1, 
+		"nplik"=>($id+1), 
+		"nuser"=>$_SESSION['ID_USER'])); 
+
 } 
+
+
 ?> 
