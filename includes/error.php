d5f4f8a96b18ce3d763c895b4bba536df5f30dbf
Pawel Kalicki <kalicki.paul@icloud.com>
2018-12-02 16:00:57 +0100

    Zapomniałem dodać znacznika php - już to zmieniłem

--- includes/error.php
+++ includes/error.php
@@ -1,5 +1,7 @@
+<?php
 if (isset($_GET['error'])){
     echo "<div class=\"notification is-danger\">";
     if ($_GET['error'] == 1 ) echo $error[] = "Brak uprawnień administratora";
     echo "</div>";
 }
+?>
