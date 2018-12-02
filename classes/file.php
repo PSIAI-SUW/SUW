dff0744a38113444602354b7d77f70ef198dedd5
Pawel Kalicki <kalicki.paul@icloud.com>
2018-12-02 15:53:19 +0100

    Reorganizacja struktury projektu

--- classes/file.php
+++ classes/file.php
@@ -29,6 +29,21 @@ class File
 
     public function downloadFile($name, $user)
     {
+      $date = date('Y-m-d H:i:s');
+
+      $sql_user = "SELECT `ID_USER` FROM `users` WHERE `login`='".$_SESSION['login']."'";
+      $id_user = $file->insertDeleteFile($sql_user);
+
+      $sql_plik = "SELECT `ID_Plik` FROM `Plik` WHERE `nazwa`='".$_POST['nazwa']."'";
+      $id_plik = $file->insertDeleteFile($sql_plik);
+
+      $sql_kurs = "SELECT `nr_kurs` FROM `Dodanie_Pliku` WHERE `nr_plik`='".$id_plik."'";
+      $id_kurs = $file->insertDeleteFile($sql_kurs);
+
+      $sql = "INSERT INTO Pobranie_Pliku (ID_Pobieranie_Pliku, data_dodania, nr_kurs, nr_plik, nr_user) VALUES (NULL, '".$date."' , '".$id_kurs."', '".$id_plik."', '".$id_user."')";
+      $result = $file->insertDeleteFile($sql);
+
+      require_once('config.php');
         if(substr($name, 0, 1) == '.')
         {
           $name = substr($name, 1);
@@ -45,11 +60,10 @@ class File
         catch(Exception $e)
         {
             echo $e;
-            /*$pdfWithWatermark = $file;
+            $pdfWithWatermark = $file;
             header("Content-Type: application/pdf");
             @readfile($pdfWithWatermark); //funkcja ktora czyta plik i go wypisuje
             exit;
-            */
         }
     }
 
