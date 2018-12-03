c004b8a4e3e8e2f5406a9d437c830d8799fc86b4
Adam Kanigowski <ka31544@zut.edu.pl>
2018-12-03 20:58:12 +0100

    Update delete.php

--- delete.php
+++ delete.php
@@ -18,7 +18,7 @@
 	if (isset($_POST['filename1'])){ //tu usuwamy plik/wykład
 		$filename1 = $_POST['filename1'];
 
-		$stmt = $db->prepare("SELECT sciezka,nr_pliku FROM plik WHERE nazwa=:name;"); // znajdz sciezke i nr pliku o tej nazwie
+		$stmt = $db->prepare("SELECT sciezka,nr_pliku FROM Plik WHERE nazwa=:name;"); // znajdz sciezke i nr pliku o tej nazwie
 		$stmt->execute(array("name"=>$filename1));
 		$row = $stmt->fetch(PDO::FETCH_ASSOC);
 		$file_to_del1 = $row['sciezka']; // sciezka pliku do usuniecia		
@@ -29,7 +29,7 @@
 			$stmt = $db->prepare("DELETE FROM Dodanie_Pliku WHERE nr_plik=:nr");
 			$stmt->execute(array("nr"=>$row['nr_pliku'])); // usuń wpis z tabeli 
 
-			$stmt = $db->prepare("DELETE FROM plik WHERE nr_pliku=:nr");
+			$stmt = $db->prepare("DELETE FROM Plik WHERE nr_pliku=:nr");
 			$stmt->execute(array("nr"=>$row['nr_pliku'])); // usuń wpis z tabeli 
             header('Location: main.php?not=15');
 		}
