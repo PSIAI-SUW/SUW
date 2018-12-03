d87c4d3f344a8275c878d1061c08073f172cc9a7
Krzysztof Kuzak <k.kuzak97@gmail.com>
2018-12-03 13:35:17 +0100

    poprawa pobierania plików

--- includes/getFilesList.php
+++ includes/getFilesList.php
@@ -2,6 +2,6 @@
   $sql = "SELECT * FROM Plik";
   $result = $file->getFileName($sql);
   foreach($result as $row){
-      echo "<a href=\"file-download.php?file_id={$row->ID_Plik}&file_name={$row->nazwa}&url={$row->sciezka}&user_id={$_SESSION['ID_USER']}\" class=\"items\">".$row->nazwa."</a>";
+      echo "<a href=\"file-download.php?file_id={$row->ID_Plik}&file_name={$row->nazwa}&url={$row->sciezka}&user_id={$_SESSION['ID_USER']}\" class=\"items\" download>".$row->nazwa."</a>";
   }
 ?>
