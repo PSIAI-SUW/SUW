d6cef5384661fed4ed90003d8c3b6eb8dfae4b95
Pawel Kalicki <kalicki.paul@icloud.com>
2018-12-03 13:02:27 +0100

    Dodanie wcięć w kodzie

--- includes/login.php
+++ includes/login.php
@@ -5,10 +5,10 @@
 	if($user->is_logged_in()){
 		if ($_SESSION["active"] != "aktywny"){
 			if ($_SESSION["type_account"] != "user"){
-      	header('Location: main.php');
+      			header('Location: main.php');
 				die();
-      }
-    }
+      		}
+    	}
 	}
 	else{
 		header('Location: index.php');
