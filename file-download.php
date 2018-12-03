724e74c69616d2d6df9adbe4131f3756c52af6bd
Pawel Kalicki <kalicki.paul@icloud.com>
2018-12-03 20:37:52 +0100

    Dodano id kursu do statystyk

--- file-download.php
+++ file-download.php
@@ -4,13 +4,14 @@
     $id_plik = htmlentities(stripslashes(trim($_GET['file_id'])), ENT_QUOTES);
     $nazwa_plik = htmlentities(stripslashes(trim($_GET['file_name'])), ENT_QUOTES);
     $id_user = htmlentities(stripslashes(trim($_GET['user_id'])), ENT_QUOTES);
+    $id_kurs = htmlentities(stripslashes(trim($_GET['kurs_id'])), ENT_QUOTES);
     $path = htmlentities(stripslashes(trim($_GET['url'])), ENT_QUOTES);
     $date = date('Y-m-d H:i:s');
 
     $file_url = $path;
 
     // statystyka
-    $sql = "INSERT INTO Pobranie_Pliku (ID_Pobieranie_Pliku, data_dodania, nr_plik, nr_user) VALUES (NULL, '".$date."' , '".$id_plik."', '".$id_user."')";
+    $sql = "INSERT INTO Pobranie_Pliku (ID_Pobieranie_Pliku, data_dodania, nr_kurs, nr_plik, nr_user) VALUES (NULL, '".$date."' , '".$id_kurs."' , '".$id_plik."', '".$id_user."')";
     $result = $file->insertDeleteFile($sql);
 
     // pobieranie pliku
