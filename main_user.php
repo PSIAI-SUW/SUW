34b30efc29a74bfed137238a6256dc3d95c678ff
Pawel Kalicki <kalicki.paul@icloud.com>
2018-12-03 11:57:20 +0100

    Zmiana układu tekstu

--- main_user.php
+++ main_user.php
@@ -15,9 +15,9 @@
 	            	<?php include 'includes/logo.html'; ?>
 	            </div>
 	            <div class="column is-6">
-								<a href="settings.php" id="settingsButton">
-									<i class="fa fa-cog fa-3x" title="Ustawienia"></i>
-								</a>
+					<a href="settings.php" id="settingsButton">
+						<i class="fa fa-cog fa-3x" title="Ustawienia"></i>
+					</a>
 	            	<a href="logout.php" id="logoutButton" class="primary-button">Wyloguj</a>
 	            </div>
 	        </div>
@@ -25,21 +25,21 @@
 		</header>
 
 		<section class="section is-medium">
-    	<main>
-        <div class="container">
+			<main>
+				<div class="container">
 					<?php include 'includes/error.php';?>
-          <div class="columns">
+					<div class="columns">
 						<div class="column is-6">
 							<h2 class="title">Kursy</h2>
 							<ul class="list-items">
 								<?php include 'includes/getCoursesList.php'; ?>
 							</ul>
 						</div>
-            <div class="column is-6">
-            	<h2 class="title">Wykłady</h2>
-              <ul class="list-items">
+						<div class="column is-6">
+							<h2 class="title">Wykłady</h2>
+							<ul class="list-items">
 								<?php include 'includes/getFilesList.php'; ?>
-              </ul>
+							</ul>
 						</div>
 					</div>
 				</div>
